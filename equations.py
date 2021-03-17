46f34b5b5541d2941c893faee184d33da9c546fc
Nivshiz <Nivshir92@gmail.com>
2021-03-17 09:40:43 +0200

    fourth

--- equations.py
+++ equations.py
@@ -42,7 +42,10 @@ def ln(x):
     while True:
         new_y = y + 2 * ((x - exponent(y)) / (x + exponent(y)))
         result = new_y
-        if abs(new_y - y) <= Epsilon:
+        dif = new_y - y
+        if dif < 0:
+            dif = dif * (-1)
+        if dif <= Epsilon:
             return result
         y = new_y
 
@@ -64,10 +67,8 @@ def sqrt(x,y):
 
 def calculate(x):
     result = exponent(x) * XtimesY(7,x) * XtimesY(x,-1) * sqrt(x,x)
-    return round(result, 6)
-
-
-
+    result = float('%0.6f' % result)
+    return result
 
 
 
